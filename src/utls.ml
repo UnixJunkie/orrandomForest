4262c8f144465f5a985c579cfc94ed979bbcb6f0
Francois BERENGER <unixjunkie@sdf.org>
2018-07-23 11:35:24 +0900

    cut long lines

--- src/utls.ml
+++ src/utls.ml
@@ -63,7 +63,8 @@ let collect_script_and_log
   if not debug then L.iter Sys.remove [r_script_fn; r_log_fn; model_fn];
   Error err_msg
 
-let read_predictions (debug: bool) (maybe_predictions_fn: Result.t): float list =
+let read_predictions (debug: bool) (maybe_predictions_fn: Result.t)
+  : float list =
   match maybe_predictions_fn with
   | Error err -> failwith err (* should have been handled by user before *)
   | Ok predictions_fn ->
