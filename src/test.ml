8cfe5d49f44a057262bab72a6109689ffb32aa60
Francois BERENGER <unixjunkie@sdf.org>
2018-07-24 19:02:38 +0900

    Test: gnuplot at the end of the regression

--- src/test.ml
+++ src/test.ml
@@ -17,31 +17,31 @@ let test_classification () =
   let sparse_data_fn = "data/train_data.csr" in
   let labels_fn = "data/train_labels.txt" in
   let preds =
-    let params = Rf.(default_params 1831 Class) in
+    let params = Rf.(default_params 1831 Classification) in
     let sparsity = Rf.Dense in
     let model =
       Rf.(train
             ~debug:true
-            Class
+            Classification
             sparsity
             params
             data_fn
             labels_fn) in
     Rf.(read_predictions
-          (predict ~debug:true Class sparsity model data_fn)) in
+          (predict ~debug:true Classification sparsity model data_fn)) in
   let sparse_preds =
-    let params = Rf.(default_params 1831 Class) in
+    let params = Rf.(default_params 1831 Classification) in
     let sparsity = Rf.Sparse 1831 in
     let model =
       Rf.(train
             ~debug:true
-            Class
+            Classification
             sparsity
             params
             sparse_data_fn
             labels_fn) in
     Rf.(read_predictions
-          (predict ~debug:true Class sparsity model sparse_data_fn)) in
+          (predict ~debug:true Classification sparsity model sparse_data_fn)) in
   assert(List.length preds = 88);
   assert(List.length sparse_preds = 88);
   let labels =
@@ -72,32 +72,32 @@ let test_regression () =
   let train_values_fn = "data/Boston_train_values.csv" in
   let test_features_fn = "data/Boston_test_features.csv" in
   let test_features_sparse_fn = "data/Boston_test_features.csr" in
-  let params = Rf.(default_params 13 Regre) in
+  let params = Rf.(default_params 13 Regression) in
   let preds =
     let sparsity = Rf.Dense in
     let model =
       Rf.(train
             ~debug:true
-            Regre
+            Regression
             sparsity
             params
             train_features_fn
             train_values_fn) in
     Rf.(read_predictions
-          (predict ~debug:true Regre sparsity model test_features_fn)) in
+          (predict ~debug:true Regression sparsity model test_features_fn)) in
   let sparse_preds =
     let train_features_sparse_fn = "data/Boston_train_features.csr" in
     let sparsity = Rf.Sparse 13 in
     let model =
       Rf.(train
             ~debug:true
-            Regre
+            Regression
             sparsity
             params
             train_features_sparse_fn
             train_values_fn) in
     Rf.(read_predictions
-          (predict ~debug:true Regre sparsity model test_features_sparse_fn)) in
+          (predict ~debug:true Regression sparsity model test_features_sparse_fn)) in
   let actual = Utls.float_list_of_file "data/Boston_test_values.csv" in
   assert(List.length actual = List.length preds);
   assert(List.length preds = 50);
@@ -105,8 +105,21 @@ let test_regression () =
   let err = rmse preds actual in
   let sparse_err = rmse sparse_preds actual in
   printf "test set RMSE: %.3f\n" err;
-  printf "sparse test set RMSE: %.3f\n" sparse_err
-  (* FBR: plot actual versus predicted and fit *)
+  printf "sparse test set RMSE: %.3f\n" sparse_err;
+  Utls.with_out_file "toplot" (fun out ->
+      L.iter (fun (x, y) ->
+          fprintf out "%f %f\n" x y
+        ) (L.combine actual preds)
+    );
+  Sys.command "\
+gnuplot --persist <<EOF\n\
+set xlabel 'actual'\n\
+set ylabel 'predicted'\n\
+f(x) = a * x + b\n\
+fit f(x) 'toplot' u 1:2 via a,b\n\
+set key left\n\
+plot 'toplot' u 1:2 t 'preds', f(x) t 'linear fit'\n\
+EOF\n"
 
 let main () =
   Log.set_log_level Log.DEBUG;
