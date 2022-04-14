c9ba33bba8ff9b00f116276e2058957033ce2512
Francois Berenger <unixjunkie@sdf.org>
2022-04-14 14:10:46 +0900

    new file:   src/model.ml

---
+++ src/model.ml
@@ -0,0 +1,127 @@
+#!/usr/bin/env ocamlscript
+Ocaml.packs := ["batteries";"minicli";"line_oriented";"orrandomForest";"cpm"]
+--
+(* train a RFC from a space-separated csv file (training set);
+   first column must be the classification label (1: class of interest; -1: other class) *)
+
+open Printf
+
+module A = BatArray
+module CLI = Minicli.CLI
+module Fn = Filename
+module L = BatList
+module LO = Line_oriented
+module Log = Dolog.Log
+module Rf = OrrandomForest.Rf
+module S = BatString
+module Utls = OrrandomForest.Utls
+
+module Score_label = struct
+  type t = bool * float (* (label, pred_score) *)
+  let get_label (l, _) = l
+  let get_score (_, s) = s
+end
+
+module ROC = Cpm.MakeROC.Make(Score_label)
+
+let run_command (cmd: string): unit =
+  Log.info "run_command: %s" cmd;
+  match Unix.system cmd with
+  | Unix.WSIGNALED _ -> (Log.fatal "run_command: signaled: %s" cmd; exit 1)
+  | Unix.WSTOPPED _ -> (Log.fatal "run_command: stopped: %s" cmd; exit 1)
+  | Unix.WEXITED i when i <> 0 ->
+    (Log.fatal "run_command: exit %d: %s" i cmd; exit 1)
+  | Unix.WEXITED _ (* i = 0 then *) -> ()
+
+let train_classifier verbose nb_features data_fn labels_fn =
+  let params: Rf.params =
+    { ntree = 100;
+      mtry = int_of_float (floor (sqrt (float nb_features)));
+      importance = true } in
+  Rf.(train ~debug:verbose
+        Classification
+        Rf.Dense
+        params
+        data_fn
+        labels_fn)
+
+let test_classifier verbose trained_model data_fn =
+  Rf.(read_predictions
+        (predict ~debug:verbose Classification Dense trained_model data_fn))
+
+let split_label_features data_csv_fn =
+  let tmp_labels_fn = Fn.temp_file ~temp_dir:"/tmp" "classif_labels_" ".csv" in
+  let tmp_features_fn = Fn.temp_file ~temp_dir:"/tmp" "classif_features_" ".csv" in
+  let cmd1 = sprintf "cut -d' ' -f1 %s > %s" data_csv_fn tmp_labels_fn in
+  let cmd2 = sprintf "cut -d' ' -f2- %s | tr ' ' '\t' > %s" data_csv_fn tmp_features_fn in
+  run_command cmd1;
+  run_command cmd2;
+  let labels = LO.lines_of_file tmp_labels_fn in
+  (* labels must be tab-separated, on a single line *)
+  LO.with_out_file tmp_labels_fn (fun out ->
+      L.iteri (fun i label ->
+          if i = 0 then
+            fprintf out "%s" label
+          else
+            fprintf out "\t%s" label
+        ) labels;
+      fprintf out "\n"
+    );
+  (tmp_features_fn, tmp_labels_fn)
+
+let () =
+  let argc, args = CLI.init () in
+  Log.(set_log_level INFO);
+  Log.color_on ();
+  if argc = 1 then
+    begin
+      eprintf "usage:\n  \
+               %s -i <filename>\n"
+        Sys.argv.(0);
+      exit 1
+    end;
+  let input_fn = CLI.get_string ["-i"] args in
+  let header, all_lines =
+    let lines = LO.lines_of_file input_fn in
+    match lines with
+    | header' :: data_lines ->
+      let rng = Random.State.make_self_init () in
+      assert(S.starts_with header' "#");
+      (S.lchop header', L.shuffle ~state:rng data_lines)
+    | _ -> failwith ("not enough lines in: " ^ input_fn) in
+  Log.info "header: %s" header;
+  let n = L.length all_lines in
+  let tmp_train_fn = Fn.temp_file ~temp_dir:"/tmp" "classif_train_" ".csv" in
+  let tmp_test_fn = Fn.temp_file ~temp_dir:"/tmp" "classif_test_" ".csv" in
+  let p = 0.8 in
+  let train_n, test_n =
+    let x = BatFloat.round_to_int (p *. (float_of_int n)) in
+    (x, n - x) in
+  Log.info "train/test: %d/%d" train_n test_n;
+  let train_lines, test_lines = L.takedrop train_n all_lines in
+  LO.lines_to_file tmp_train_fn train_lines;
+  LO.lines_to_file tmp_test_fn test_lines;
+  let train_features_fn, train_labels_fn = split_label_features tmp_train_fn in
+  let nb_features = S.count_char (L.hd all_lines) ' ' in
+  Log.info "|features|=%d" nb_features;
+  let model = train_classifier true nb_features train_features_fn train_labels_fn in
+  let feat_importance = Rf.read_predictions (Rf.get_features_importance model) in
+  let index2feature_name = A.of_list (L.tl (S.split_on_char ' ' header)) in
+  assert(L.length feat_importance = A.length index2feature_name);
+  assert(A.length index2feature_name = nb_features);
+  L.iteri (fun i imp ->
+      Log.info "imp(%s): %.2f" index2feature_name.(i) imp
+    ) feat_importance;
+  let test_features_fn, test_labels_fn = split_label_features tmp_test_fn in
+  let test_preds = test_classifier true model test_features_fn in
+  let test_labels =
+    (* all labels are on a single line in the labels file *)
+    let tab_separated = L.hd (LO.lines_of_file test_labels_fn) in
+    let label_strings = S.split_on_char '\t' tab_separated in
+    L.map (function
+        | "1" -> true
+        | "-1" -> false
+        | other -> failwith other
+      ) label_strings in
+  let auc = ROC.auc (L.combine test_labels test_preds) in
+  printf "AUC: %.3f\n" auc
