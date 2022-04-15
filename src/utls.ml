5fa31cb9962e5d369e3176edd07442caaa3842c1
Francois Berenger <unixjunkie@sdf.org>
2022-04-15 15:26:17 +0900

    Utls: doesn't depend on batteries nor line_oriented

--- src/utls.ml
+++ src/utls.ml
@@ -1,6 +1,5 @@
 open Printf
 
-module L = List
 module Log = Dolog.Log
 
 let with_in_file fn f =
@@ -46,7 +45,7 @@ let float_list_of_file fn =
              nan) in
         pred :: acc
       ) [] in
-  L.rev res
+  List.rev res
 
 type filename = string
 
@@ -61,7 +60,7 @@ let collect_script_and_log
   bprintf buff "--- %s ---\n" r_log_fn;
   append_file_to_buffer buff r_log_fn;
   let err_msg = Buffer.contents buff in
-  if not debug then L.iter Sys.remove [r_script_fn; r_log_fn; model_fn];
+  if not debug then List.iter Sys.remove [r_script_fn; r_log_fn; model_fn];
   Error err_msg
 
 let read_predictions (debug: bool) (maybe_predictions_fn: Result.t)
@@ -72,3 +71,23 @@ let read_predictions (debug: bool) (maybe_predictions_fn: Result.t)
     if debug then Log.debug "%s" predictions_fn;
     (* we did not create that file; we are not responsible for it and will not delete it *)
     (float_list_of_file predictions_fn, predictions_fn)
+
+let run_command ?(debug = false) (cmd: string): unit =
+  if debug then Log.info "run_command: %s" cmd;
+  match Unix.system cmd with
+  | Unix.WSIGNALED _ -> (Log.fatal "run_command: signaled: %s" cmd; exit 1)
+  | Unix.WSTOPPED _ -> (Log.fatal "run_command: stopped: %s" cmd; exit 1)
+  | Unix.WEXITED i when i <> 0 ->
+    (Log.fatal "run_command: exit %d: %s" i cmd; exit 1)
+  | Unix.WEXITED _ (* i = 0 then *) -> ()
+
+let array_to_file (fn: filename) (f: 'a -> string) (a: 'a array): unit =
+  with_out_file fn (fun out ->
+      Array.iter (fun x ->
+          fprintf out "%s\n" (f x)
+        ) a
+    )
+
+let int_of_bool = function
+  | true -> 1
+  | false -> 0
